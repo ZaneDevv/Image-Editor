fab7c47d491094015d6d9a4bbbbd76014831a53a
ZaneDev <alvferbarrero@gmail.com>
2025-05-27 10:12:36 +0200

    Gallery updated to be able to get all saved pictures

--- Gallery/Scripts/CreatePictureInGallery.js
+++ Gallery/Scripts/CreatePictureInGallery.js
@@ -46,58 +46,62 @@ function CreatePicture() {
 
 function SetUpGallery() {
     fetch("./Scripts/ReadSavedFile.php")
-    .then(response => response.text())
-    .then(text => {
+    .then(response => response.json())
+    .then(pictures => {
 
-        const Elements = CreatePicture();
+        if (!Array.isArray(pictures)) {  return; }
 
-        Elements.Text.innerHTML = "Test";
-        Elements.InsideCanvas.innerHTML = text;
-
-        const ComputedStyleImage = getComputedStyle(Elements.InsideCanvas.children[0]);
-        const ComputedStyleParentDiv = getComputedStyle(Elements.ImageDiv);
-
-        const ImageWidth = parseInt(ComputedStyleImage.width, 10);
-        const ImageHeight = parseInt(ComputedStyleImage.height, 10);
-
-        const ParentDivWidth = parseInt(ComputedStyleParentDiv.width, 10);
-        const ParentDivHeight = parseInt(ComputedStyleParentDiv.height, 10);
-        
-        const ScaleX = ParentDivWidth / ImageWidth;
-        const ScaleY = ParentDivHeight / ImageHeight;
-
-        const ScaleFactor = Max(ScaleX, ScaleY);
-
-        Elements.InsideCanvas.style.transform = `scale(${ScaleFactor})`;
-        Elements.InsideCanvas.style.left = 0;
-        Elements.InsideCanvas.style.top = 0;
-        Elements.InsideCanvas.style.boxShadow = "0px 0px 0px 0px #00000000";
-        Elements.InsideCanvas.style.position = "absolute";
-
-        Elements.MainDiv.addEventListener("mouseup", function(){
-            SetDarkScreen();
-
+        pictures.forEach(picture => {
+            const Elements = CreatePicture();
+    
+            Elements.Text.innerHTML = picture.Name;
+            Elements.InsideCanvas.innerHTML = picture.Content;
+    
             const ComputedStyleImage = getComputedStyle(Elements.InsideCanvas.children[0]);
-            const ComputedStyleParentDiv = getComputedStyle(DarkScreen);
-
+            const ComputedStyleParentDiv = getComputedStyle(Elements.ImageDiv);
+    
             const ImageWidth = parseInt(ComputedStyleImage.width, 10);
             const ImageHeight = parseInt(ComputedStyleImage.height, 10);
-
+    
             const ParentDivWidth = parseInt(ComputedStyleParentDiv.width, 10);
             const ParentDivHeight = parseInt(ComputedStyleParentDiv.height, 10);
             
             const ScaleX = ParentDivWidth / ImageWidth;
             const ScaleY = ParentDivHeight / ImageHeight;
-
+    
             const ScaleFactor = Max(ScaleX, ScaleY);
-
-            const newCanvas = Elements.InsideCanvas.cloneNode(true);
-            newCanvas.style.position = "absolute";
-            newCanvas.style.transform = `scale(${ScaleFactor}) translate(-50%, -50%)`;
-            newCanvas.style.left = "50%";
-            newCanvas.style.top = "50%";
-            DarkScreen.appendChild(newCanvas);
-        })
+    
+            Elements.InsideCanvas.style.transform = `scale(${ScaleFactor})`;
+            Elements.InsideCanvas.style.left = 0;
+            Elements.InsideCanvas.style.top = 0;
+            Elements.InsideCanvas.style.boxShadow = "0px 0px 0px 0px #00000000";
+            Elements.InsideCanvas.style.position = "absolute";
+    
+            Elements.MainDiv.addEventListener("mouseup", function(){
+                SetDarkScreen();
+    
+                const ComputedStyleImage = getComputedStyle(Elements.InsideCanvas.children[0]);
+                const ComputedStyleParentDiv = getComputedStyle(DarkScreen);
+    
+                const ImageWidth = parseInt(ComputedStyleImage.width, 10);
+                const ImageHeight = parseInt(ComputedStyleImage.height, 10);
+    
+                const ParentDivWidth = parseInt(ComputedStyleParentDiv.width, 10);
+                const ParentDivHeight = parseInt(ComputedStyleParentDiv.height, 10);
+                
+                const ScaleX = ParentDivWidth / ImageWidth;
+                const ScaleY = ParentDivHeight / ImageHeight;
+    
+                const ScaleFactor = Max(ScaleX, ScaleY);
+    
+                const newCanvas = Elements.InsideCanvas.cloneNode(true);
+                newCanvas.style.position = "absolute";
+                newCanvas.style.transform = `scale(${ScaleFactor}) translate(-50%, -50%)`;
+                newCanvas.style.left = "50%";
+                newCanvas.style.top = "50%";
+                DarkScreen.appendChild(newCanvas);
+            })
+        });
 
     })
     .catch(err => {
--- Gallery/Scripts/ReadSavedFile.php
+++ Gallery/Scripts/ReadSavedFile.php
@@ -1,9 +1,29 @@
 <?php
 
-    $file_path = "../../Saved/testing.txt"; 
+    $file_path = "../../Saved/"; 
 
-    if (file_exists($file_path) ) {
-        echo file_get_contents($file_path);
+    if (is_dir($file_path)) {
+        $children = scandir($file_path);
+        $children = array_diff($children, array('.', '..'));
+        
+        $files_info = array();
+        
+        foreach ($children as $child) {
+            $full_path = $file_path . $child;
+
+            if (is_file($full_path)) {
+                $file_contents = file_get_contents($full_path);
+                $files_info[] = array(
+                    'Name' => pathinfo($child, PATHINFO_FILENAME),
+                    'Content' => $file_contents
+                );
+            }
+        }
+
+        echo json_encode($files_info); 
+    }
+    else {
+        echo json_encode([]);
     }
 
 ?>
